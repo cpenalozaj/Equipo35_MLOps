e4da0ce8cce974e8abcc30640494bd1ee237c4a9
Moises Diaz <moisesdiazmalagon@gmail.com>
2024-11-16 00:19:58 -0600

    Adds literal typings for model serving

--- server/models/student.py
+++ server/models/student.py
@@ -1,23 +1,24 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class StudentData(BaseModel):
-    ge: str
-    cst: str
-    tnp: str
-    twp: str
-    iap: str
-    arr: str
-    ls: str
-    as_: str  # renamed "as" to "as_" to avoid using a reserved keyword
-    fmi: str
-    fs: str
-    fq: str
+    ge: Literal["F", "M"]
+    cst: Literal["G", "OBC", "MOBC", "ST", "SC"]
+    tnp: Literal["Good", "Vg", "Pass", "Best"]
+    twp: Literal["Good", "Vg", "Pass", "Best"]
+    iap: Literal["Vg", "Good", "Pass", "Best"]
+    arr: Literal["Y", "N"]
+    ls: Literal["V", "T"]
+    as_: Literal["Paid", "Free"]   # renamed "as" to "as_" to avoid using a reserved keyword
+    fmi: Literal["Medium", "Low", "Am", "High", "Vh"]
+    fs: Literal["Average", "Small", "Large"]
+    fq: Literal["Um", "12", "10", "Il", "Degree", "Pg"]
     mq: int
-    fo: str
-    mo: str
-    nf: str
-    sh: str
-    ss: str
-    me: str
-    tt: str
-    atd: str
+    fo: Literal["Farmer", "Service", "Business", "Others", "Retired"]
+    mo: Literal["Housewife", "Service", "Business", "Retired", "Others"]    
+    nf: Literal["Small", "Average", "Large"]
+    sh: Literal["Poor", "Average", "Good"]
+    ss: Literal["Govt", "Private"]
+    me: Literal["Asm", "Eng", "Hin", "Ben"]
+    tt: Literal["Small", "Average", "Large"]
+    atd: Literal["Good", "Average", "Poor"]
